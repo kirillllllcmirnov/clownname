86cb3e6d48df37f4b4403d89b5b1accbac240296
kirillllllcmirnov <kiryusha.smirnov@internet.ru>
2024-05-12 00:32:06 +0300

    master

--- Content.Server/Cluwne/ClownNameSystem.cs
+++ Content.Server/Cluwne/ClownNameSystem.cs
@@ -1,6 +1,7 @@
 using Content.Server.Administration;
 using Content.Server.GameTicking;
 using Content.Server.Access.Systems;
+using Content.Server.StationRecords.Systems;
 namespace Content.Server.Cluwne;
 
 public sealed class ClownNameSystem : EntitySystem
@@ -8,6 +9,7 @@ public sealed class ClownNameSystem : EntitySystem
     [Dependency] private readonly MetaDataSystem _data = default!;
     [Dependency] private readonly IdCardSystem _card = default!;
     [Dependency] private readonly QuickDialogSystem _dialog = default!;
+    [Dependency] private readonly StationRecordsSystem _record = default!;
 
     public override void Initialize()
     {
@@ -15,17 +17,24 @@ public sealed class ClownNameSystem : EntitySystem
         SubscribeLocalEvent<ClownNameComponent, PlayerSpawnCompleteEvent>(OnInit);
     }
     public void OnInit(EntityUid uid,ClownNameComponent comp ,  PlayerSpawnCompleteEvent init){
-        if(!TryComp<ActorComponent>(uid,out var actor))
-            return;
-        if(!TryComp<MetaDataComponent>(uid,out var data))
-            return;
-        if(!_card.TryFindIdCard(uid, out var card))
-            return;
+        if(!TryComp<ActorComponent>(uid,out var actor)){return;}
+        if(!TryComp<MetaDataComponent>(uid,out var data)){return;}
+        if(!_card.TryFindIdCard(uid, out var card)){return;}
         _dialog.OpenDialog(actor.PlayerSession,"Твое имя","Введи свое имя",(string name)=>{
-            if(name!="" && _card.TryChangeFullName(card,name)){
-                _data.SetEntityName(uid,name,data);
+            if(name=="" )return;
+
+            if (!TryComp<StationRecordKeyStorageComponent>(card, out var keyStorage)
+                || keyStorage.Key is not { } key
+                || !_record.TryGetRecord<GeneralStationRecord>(key, out var record))
+            {
+                return;
             }
-        });
+
+            _card.TryChangeFullName(card,name);
+            _data.SetEntityName(uid,name,data);
+            record.Name=name;
+            _record.Synchronize(key);
+            });
     }
 
 }
