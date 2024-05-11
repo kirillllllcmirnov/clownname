8866cf8e59d7b39f1a6dd564b34d2cacc6aba01d
kirillllllcmirnov <kiryusha.smirnov@internet.ru>
2024-05-11 20:03:18 +0300

    master

--- Content.Server/Cluwne/ClownNameSystem.cs
+++ Content.Server/Cluwne/ClownNameSystem.cs
@@ -1,7 +1,5 @@
-using Robust.Shared.Player;
 using Content.Server.Administration;
 using Content.Server.GameTicking;
-using Content.Shared.Access.Systems;
 using Content.Server.Access.Systems;
 namespace Content.Server.Cluwne;
 
@@ -9,20 +7,22 @@ public sealed class ClownNameSystem : EntitySystem
 {
     [Dependency] private readonly MetaDataSystem _data = default!;
     [Dependency] private readonly IdCardSystem _card = default!;
-
     [Dependency] private readonly QuickDialogSystem _dialog = default!;
+
     public override void Initialize()
     {
         base.Initialize();
         SubscribeLocalEvent<ClownNameComponent, PlayerSpawnCompleteEvent>(OnInit);
     }
     public void OnInit(EntityUid uid,ClownNameComponent comp ,  PlayerSpawnCompleteEvent init){
-        if(!TryComp<ActorComponent>(uid,out var actor)){return;}
-        if(!TryComp<MetaDataComponent>(uid,out var data)){return;}
-        if(!_card.TryFindIdCard(uid, out var card)){return;}
+        if(!TryComp<ActorComponent>(uid,out var actor))
+            return;
+        if(!TryComp<MetaDataComponent>(uid,out var data))
+            return;
+        if(!_card.TryFindIdCard(uid, out var card))
+            return;
         _dialog.OpenDialog(actor.PlayerSession,"Твое имя","Введи свое имя",(string name)=>{
             if(name!="" && _card.TryChangeFullName(card,name)){
-
                 _data.SetEntityName(uid,name,data);
             }
         });
